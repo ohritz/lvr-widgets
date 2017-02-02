98234c233aa8c8f9ff6da10f11f6e3f118eba7ff
Sohan Fernando <sohan.fernando@registercentrum.se>
2017-02-02 16:03:08 +0100

    cleanup of potential dev server

--- dev-server.js
+++ dev-server.js
@@ -7,39 +7,11 @@ const fs = require('fs');
 const port = process.env.DEV_PORT || 3005;
 const baseUrl = '';
 
-const codeToNameMap = {
-    1274: 'EffectivenessGrid',
-    1275: 'IndicatorsForManagement',
-    1276: 'IndicatorsForHospital',
-    1322: 'IndicatorsForQuarters',
-    2378: 'IndicatorsOverTime'
-}
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
-
-app.get('/api/metadata/pages/:pageId', function (req, res) {    
-    const pageId = req.params.pageId;
-    console.log(` serving widget number ${pageId}, as ${codeToNameMap[pageId]}`);
-    fs.readFile(`src/${pageId}.html`, 'utf-8', function (err, htmlFile) {
-        if (err)
-            return res.status(401).end(err);
-
-        fs.readFile(`src/js/views/${codeToNameMap[pageId]}.js`, 'utf-8', function (err, jsFile) {
-            if (err)
-                return res.status(401).end(err);
-            const result = `${htmlFile}<script>${jsFile}</script>`;
-            return res.status(200).end(JSON.stringify({
-                data: {
-                    PageContent: result,
-                    PageID: pageId
-                }
-            }));
-        });
-    })
 });
 
 app.listen(port, function () {
