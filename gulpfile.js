0c49fc6fbe736778370c65a8370051db706042ed
Sohan Fernando <sohan.fernando@registercentrum.se>
2017-02-03 08:59:33 +0100

    build tasks implemented

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,18 @@ const inject = require('gulp-inject');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const es = require('event-stream');
+const order = require('gulp-order');
+const cleancss = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const del = require('del');
 
-let apiServer = null;
+var apiServer = null;
 
 config = {
     js: './src/js/**/*.js',
     css: './src/css/**/*.css',
     html: './src/**/*.html',
+    src: './src',
     dist: './dist'
 }
 
@@ -24,41 +29,53 @@ gulp.task('start:api', (cb) => {
     cb();
 });
 
-// gulp.task('styles:dev', () => {
-//     return gulp.src([config.css]);
-// });
-
-// gulp.task('scripts:dev', () => {
-//     return gulp.src([config.js]);
-// });
-
 gulp.task('inject:dev', () => {
-    var sources = gulp.src([config.css, config.js], {read: false});
-    return gulp.src('./src/index.html')
-    .pipe(inject(sources, {relative: true}))
-    .pipe(gulp.dest('./src'));
+    var sources = gulp.src([config.css, config.js, `!${config.src}/js/StatKOLSV.js`, `!${config.src}/js/widget.js`], {
+        read: false
+    });
+    return gulp.src(config.src + '/index.html')
+        .pipe(inject(sources, {
+            relative: true
+        }))
+        .pipe(gulp.dest(config.src));
 });
 
-gulp.task('styles:build', () => {
+gulp.task('clean:build', () => {
+    return del([config.dist + '/*'])
+        .then(paths => console.log('Deleting:\n', paths.join('\n')));
+});
+
+gulp.task('styles:build', ['clean:build'], () => {
     return gulp.src([config.css])
+        .pipe(cleancss({
+            compatibility: 'ie8'
+        }))
         .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('scripts:build', () => {
-    return gulp.src([config.js])
+gulp.task('scripts:build', ['clean:build'], () => {
+    return gulp.src([config.js, `!${config.src}/js/StatKOLSV.js`, `!${config.src}/js/widget.js`])
+        .pipe(sourcemaps.init())
         .pipe(concat('gaugeWidjet.js'))
         .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(config.dist));
 });
+
+
 
 gulp.task('inject:build', ['scripts:build', 'styles:build'], () => {
-    var sources = gulp.src([config.dist + '/**/*.css', config.dist + '**/*.js'], {read: false});
+    var sources = gulp.src([config.dist + '/**/*.css', config.dist + '**/*.js'], {
+        read: false
+    });
     return gulp.src('./src/index.html')
-        .pipe(inject(sources,{relative: true}))
+        .pipe(inject(sources, {
+            relative: true
+        }))
         .pipe(gulp.dest(config.dist));
 });
 
-gulp.task('serve:build', ['inject:build'],() => {
+gulp.task('serve:build', ['inject:build'], () => {
     browserSync.init({
         server: {
             port: 3005,
@@ -67,7 +84,9 @@ gulp.task('serve:build', ['inject:build'],() => {
     })
 });
 
-gulp.task('serve:dev',['inject:dev'] ,(cb) => {
+gulp.task('build', ['inject:build']);
+
+gulp.task('serve:dev', ['inject:dev', 'watch:dev'], (cb) => {
     browserSync.init({
         server: {
             port: 3005,
@@ -76,8 +95,9 @@ gulp.task('serve:dev',['inject:dev'] ,(cb) => {
     });
     cb();
 });
-gulp.task('watch', ['serve:dev'], () => {
-    gulp.watch('./src/**/*').on('change', browserSync.reload);
+gulp.task('watch:dev', () => {
+    gulp.watch(config.js).on('change', browserSync.reload);
+    gulp.watch(config.css).on('change', browserSync.stream);
 });
 
 process.on('exit', function () {
@@ -85,4 +105,4 @@ process.on('exit', function () {
     apiServer && apiServer.kill();
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['serve:dev']);
