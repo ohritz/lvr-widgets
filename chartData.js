586c7308564f32a244b829e945fcf7179262a28c
Sohan Fernando <sohan.fernando@registercentrum.se>
2017-02-06 09:24:33 +0100

    chartdata for dev-server

---
+++ chartData.js
@@ -0,0 +1,18 @@
+module.exports = function (id) {
+    return {
+        id: id,
+        data: [{
+                unit: 'one',
+                value: 2056
+            },
+            {
+                unit: 'two',
+                value: 3053
+            },
+            {
+                unit: 'three',
+                value: 1045
+            }
+        ]
+    };
+}
--- dev-server.js
+++ dev-server.js
@@ -3,39 +3,32 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const guagedata = require('./guageData');
+const guageData = require('./guageData');
+const chartData = require('./chartData');
 const port = process.env.DEV_PORT || 3005;
 const baseUrl = '';
 
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.get('/stratum/api/statistics/lvr/gaugeWidget', function(req,res) {
+app.get('/stratum/api/statistics/lvr/gaugeWidget', function (req, res) {
   console.log('stop hitting me');
-  var data = guagedata(6);
+  var id = req.query.id,
+    data;
+    console.log('id is: ',id);
+  if (typeof id === 'undefined')
+    data = guageData(6);
+  else
+    data = chartData(id);
   res.status(200).send(data);
 
 });
 
-app.get('stratum/api/statistics/lvr/gaugeWidget/:id/details', function(req, res) {
-    var id = re.id;
-    res.status(200).send({
-      id: id,
-      data: [
-        {unit: 'one',
-      value: 2056},
-      {unit: 'two',
-      value: 3053},
-      {unit: 'three',
-      value: 1045}
-      ]
-    })
-});
 
 app.listen(port, function () {
-    console.log(`listening on ${port}, for api calls.`);
+  console.log(`listening on ${port}, for api calls.`);
 })
