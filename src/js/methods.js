891b64d1e1ee3709e9b823557ddf53f0f82b4716
Sohan Fernando <sohan.fernando@registercentrum.se>
2017-02-06 09:24:20 +0100

    added general methods

--- src/js/methods.js
+++ src/js/methods.js
@@ -1,7 +1,30 @@
 (function (Repository) {
     (function (Local) {
         Local.Methods = {
-            noOp: function(){}
+            noOp: function(){},
+            getChartData: function getChartdata(identifier, callback) {
+                var baseUrl = '/stratum/api/statistics/lvr/gaugeWidget?id=';
+                
+                // todo: this url and implementation will change after api is finalized.
+                var url = baseUrl + identifier;
+                Ext.Ajax.request({
+                    method: 'get',
+                    url: url,
+                    success: function(res) {
+                        var data = Ext.decode(res.responseText);
+                        callback(null, data);                        
+                    },
+                    failure: function(err) {
+                        callback(err);
+                    }
+                })
+            },
+            scrollToElement: function (element) {
+                if (!element || !element.getY) {
+                    return;
+                }
+                (Ext.isChrome ? Ext.getBody() : Ext.get(document.documentElement)).scrollTo('top', element.getY(), true);
+            }
         }
     })(Repository.Local = Repository.Local || {});
 }(window.Repository = window.Repository || {}));
